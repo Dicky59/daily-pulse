582d108b1458b2d0d12c1835527915cf4c986d0d
Dicky59 <pekkaarvolaine@gmail.com>
2025-12-11 16:15:27 +0200

    cursor rules updated

--- app/src/main/java/com/dicky59/dailypulse/articles/data/ArticleRepository.kt
+++ app/src/main/java/com/dicky59/dailypulse/articles/data/ArticleRepository.kt
@@ -2,7 +2,9 @@ package com.dicky59.dailypulse.articles.data
 
 import com.dicky59.dailypulse.BuildConfig
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class ArticleRepository
 @Inject
 constructor(
